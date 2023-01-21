13a051f59b92174fe33148524cf1a05fde34a8bd
SergeiRudakov <rudakov-sv@yandex.ru>
2023-01-21 14:22:52 +0200

    Rename DrawVerifier to CellVerifier

--- src/tictactoe/Launcher.java
+++ src/tictactoe/Launcher.java
@@ -4,7 +4,7 @@ import tictactoe.component.*;
 
 public class Launcher {
     public static void main(final String[] args) {
-        final Game game = new Game(new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new DrawVerifier());
+        final Game game = new Game(new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new CellVerifier());
         game.play();
     }
 }
--- src/tictactoe/component/Game.java
+++ src/tictactoe/component/Game.java
@@ -9,14 +9,14 @@ public class Game {
     private final ComputerMove computerMove;
     private final UserMove userMove;
     private final WinnerVerifier winnerVerifier;
-    private final DrawVerifier drawVerifier;
+    private final CellVerifier cellVerifier;
 
-    public Game(final DataPrinter dataPrinter, final ComputerMove computerMove, final UserMove userMove, final WinnerVerifier winnerVerifier, final DrawVerifier drawVerifier) {
+    public Game(final DataPrinter dataPrinter, final ComputerMove computerMove, final UserMove userMove, final WinnerVerifier winnerVerifier, final CellVerifier cellVerifier) {
         this.dataPrinter = dataPrinter;
         this.computerMove = computerMove;
         this.userMove = userMove;
         this.winnerVerifier = winnerVerifier;
-        this.drawVerifier = drawVerifier;
+        this.cellVerifier = cellVerifier;
     }
 
     public void play() {
@@ -34,7 +34,7 @@ public class Game {
                 System.out.println("YOU WIN");
                 break;
             }
-            if (drawVerifier.isDraw(gameTable)) {
+            if (cellVerifier.allCellsFilled(gameTable)) {
                 System.out.println("Sorry, DRAW!");
                 break;
             }
@@ -44,7 +44,7 @@ public class Game {
                 System.out.println("COMPUTER WIN");
                 break;
             }
-            if (drawVerifier.isDraw(gameTable)) {
+            if (cellVerifier.allCellsFilled(gameTable)) {
                 System.out.println("Sorry, DRAW!");
                 break;
             }
