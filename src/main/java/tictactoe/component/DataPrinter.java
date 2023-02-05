58f1ab9ea81139197d014c105be36a81d25df4a6
SergeiRudakov <rudakov-sv@yandex.ru>
2023-02-05 13:04:22 +0200

    Add lambda expression for DataPrinter

--- src/main/java/tictactoe/component/DataPrinter.java
+++ src/main/java/tictactoe/component/DataPrinter.java
@@ -12,26 +12,27 @@ public class DataPrinter {
     }
 
     public void printMappingTable() {
+        print((i,j) -> String.valueOf(cellNumberConverter.toNumber(new Cell(i,j))));
+    }
+
+    public void printGameTable(final GameTable gameTable) {
+        print((i,j) -> gameTable.getSign(new Cell(i, j)).toString());
+    }
+
+    private void print(final Lambda lambda) {
         String horizontalSplitter = "-------------";
         for (int i = 0; i < 3; i++) {
             System.out.println(horizontalSplitter);
             for (int j = 0; j < 3; j++) {
-                System.out.print("| " + cellNumberConverter.toNumber(new Cell(i,j)) + " ");
+                System.out.print("| " + lambda.getValue(i, j) + " ");
             }
             System.out.println("|");
         }
         System.out.println(horizontalSplitter);
     }
 
-    public void printGameTable(final GameTable gameTable) {
-        String horizontalSplitter = "-------------";
-        for (int i = 0; i < 3; i++) {
-            System.out.println(horizontalSplitter);
-            for (int j = 0; j < 3; j++) {
-                System.out.print("| " + gameTable.getSign(new Cell(i, j)).toString() + " ");
-            }
-            System.out.println("|");
-        }
-        System.out.println(horizontalSplitter);
+    @FunctionalInterface
+    private interface Lambda {
+        String getValue(int i, int j);
     }
 }
